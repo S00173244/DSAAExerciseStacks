ddb1f5ad451fb8042e0ee33514dab8e4980b66dc
S00173244 <nels.puthoor@mail.itsligo.ie>
2018-02-20 12:03:41 +0000

    Need to update

---
+++ Game1/Game1/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helpers
+{
+    public enum ActiveScreenState { OPENING, PLAY, PAUSE, ENDING };
+    public static class Helper
+    {
+        
+        public static SpriteFont GameFont;
+        public static GraphicsDevice graphicsDevice;
+        public static Vector2 WorldBounds;
+    }
+}
